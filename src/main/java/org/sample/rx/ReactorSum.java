4294774224c03c71190edd71010353cd66f875ec
leoncn <leoncn@me.com>
2016-01-17 22:13:56 +0800

    reactor sum

--- src/main/java/org/sample/rx/ReactorSum.java
+++ src/main/java/org/sample/rx/ReactorSum.java
@@ -3,13 +3,18 @@ package org.sample.rx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.observables.ConnectableObservable;
+import rx.observers.SafeSubscriber;
 import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
@@ -34,59 +39,62 @@ public class ReactorSum {
                 while (!subscriber.isUnsubscribed() && (line = reader.readLine()) != null) {
                     subscriber.onNext(line);
                 }
-                subscriber.onCompleted();
+
+                if (!subscriber.isUnsubscribed()) {
+
+                    subscriber.onCompleted();
+                }
             } catch (IOException e) {
                 subscriber.onError(e);
             }
         });
-                //.publish();
-//
-       Observable o = Observable.create((Subscriber<? super String> subscriber) -> {
-           if (subscriber.isUnsubscribed())
-               return;
-           BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-           String line = null;
+        ConnectableObservable<String> notEmitUntilConn = lineInput.publish();
 
 
-           try {
-               while (!subscriber.isUnsubscribed() && (line = reader.readLine()) != null) {
-                   log.info(line);
-                   subscriber.onNext(line);
-               }
-               subscriber.onCompleted();
-           } catch (IOException e) {
-               subscriber.onError(e);
-           }
-           subscriber.onCompleted();
-               });
+        Function<Pattern, Observable<Integer>> lineParser = (pattern) ->
+                notEmitUntilConn
+                        .filter(line ->
+                                {
+                                    return pattern.matcher(line).matches();
+                                }
+                        )
+                        .map(line ->
+                                {
+                                    return Integer.parseInt(line.substring(line.indexOf("=") + 1).trim());
+                                }
+                        );
 
-        o.subscribeOn(Schedulers.computation()).subscribe(log::info);
-        o.subscribe(log::error);
+        Subscriber<Integer> sub = new SafeSubscriber<Integer>(new Subscriber<Integer>() {
 
-//        Function<Pattern, Observable<Integer>> lineParser = (pattern) -> {
-//            Observable<Integer> obs = lineInput.filter(line ->
-//            {
-//                System.out.println(pattern.toString());
-//                return pattern.matcher(line).matches();
-//            }
-//
-//            ).map(line ->
-//                    {
-//                      return Integer.parseInt(line.substring(line.indexOf("=") + 1).trim());
-//                    }
-//            );
-//            return obs;
-//        };
-//
-//        Observable.combineLatest(
-//                lineParser.apply(Pattern.compile("^a.*")),
-//                lineParser.apply(Pattern.compile("^b.*")),
-//                (x, y) -> x + y
-//        ).subscribe(
-//                System.out::println
-//        );
+            @Override
+            public void onCompleted() {
 
-     //   lineInput.connect();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(Integer s) {
+                System.out.println(s);
+            }
+        });
+
+        sub.add(Subscriptions.create( () -> {
+            System.out.println("unsubscribe callback");
+        }));
+
+
+        Observable.combineLatest(
+                lineParser.apply(Pattern.compile("^a.*")),
+                lineParser.apply(Pattern.compile("^b.*")),
+                (x, y) -> x + y
+        ).subscribe(sub);
+
+
+        notEmitUntilConn.connect();
     }
 }
